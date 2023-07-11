c625a4db59685faa7026ab8de7b4b23a47c21609
Dian DInihari | Tia <55896324+ddhyuliani@users.noreply.github.com>
2023-07-11 13:05:42 +0700

    completed project

--- UIKitFundamental/MentorDataTableViewController.swift
+++ UIKitFundamental/MentorDataTableViewController.swift
@@ -6,84 +6,95 @@
 //
 
 import UIKit
+import CoreData
+
+var mentor = [MentorData]()
 
 class MentorDataTableViewController: UITableViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    var firstLoad = true
+    
+    func nonDeletedMentor() -> [MentorData] {
+        var noDeleteMentorList = [MentorData]()
+        for data in mentor
+        {
+            if(data.deletedDate == nil)
+            {
+                noDeleteMentorList.append(data)
+            }
+        }
+        return noDeleteMentorList
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    
+    override func viewDidLoad()
+    {
+        if(firstLoad)
+        {
+            firstLoad = false
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context: NSManagedObjectContext = appDelegate.persistentContainer.viewContext
+            let request = NSFetchRequest<NSFetchRequestResult>(entityName: "MentorData")
+            do {
+                let results:NSArray = try context.fetch(request) as NSArray
+                for result in results
+                {
+                    let note = result as! MentorData
+                    mentor.append(note)
+                }
+            }
+            catch
+            {
+                print("Fetch Failed")
+            }
+        }
     }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let mentorCell = tableView.dequeueReusableCell(withIdentifier: "mentorCell", for: indexPath) as! MentorTableViewCell
+        
+        let thisData: MentorData!
+        thisData = nonDeletedMentor()[indexPath.row]
+        
+        mentorCell.nameLabel.text = thisData.name
+        mentorCell.originCity.text = thisData.city
+        
+        return mentorCell
     }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return nonDeletedMentor().count
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func viewDidAppear(_ animated: Bool)
+    {
+        tableView.reloadData()
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+        self.performSegue(withIdentifier: "editMentor", sender: self)
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+    {
+        if(segue.identifier == "editMentor")
+        {
+            let indexPath = tableView.indexPathForSelectedRow!
+            
+            let noteDetail = segue.destination as? ViewController
+            
+            let selectedNote : MentorData!
+            selectedNote = nonDeletedMentor()[indexPath.row]
+            noteDetail!.selectedMentor = selectedNote
+            
+            tableView.deselectRow(at: indexPath, animated: true)
+        }
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
+    
 
 }
--- UIKitFundamental/ViewController.swift
+++ UIKitFundamental/ViewController.swift
@@ -6,14 +6,120 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var originLabel: UILabel!
+    @IBOutlet weak var cityPicker: UIPickerView!
+    
+    var selectedMentor: MentorData? = nil
+    let city = ["Bogor", "Surabaya", "Bandung", "Pamulang", "Cirebon", "Kupang"]
+    
+    override func viewDidLoad(){
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        cityPicker.delegate = self
+        cityPicker.dataSource = self
+        
+        if(selectedMentor != nil)
+        {
+            nameField.text = selectedMentor?.name
+            originLabel.text = selectedMentor?.city
+        }
     }
 
+    @IBAction func saveAction(_ sender: Any) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context: NSManagedObjectContext = appDelegate.persistentContainer.viewContext
+        if(selectedMentor == nil)
+        {
+            let entity = NSEntityDescription.entity(forEntityName: "MentorData", in: context)
+            let newNote = MentorData(entity: entity!, insertInto: context)
+            newNote.id = Int32(truncating: mentor.count as NSNumber)
+            newNote.name = nameField.text
+            newNote.city = originLabel.text
+            do
+            {
+                try context.save()
+                mentor.append(newNote)
+                navigationController?.popViewController(animated: true)
+            }
+            catch
+            {
+                print("context save error")
+            }
+        }
+        else //edit
+        {
+            let request = NSFetchRequest<NSFetchRequestResult>(entityName: "MentorData")
+            do {
+                let results:NSArray = try context.fetch(request) as NSArray
+                for result in results
+                {
+                    let note = result as! MentorData
+                    if(note == selectedMentor)
+                    {
+                        note.name = nameField.text
+                        note.city = originLabel.text
+                        try context.save()
+                        navigationController?.popViewController(animated: true)
+                    }
+                }
+            }
+            catch
+            {
+                print("Fetch Failed")
+            }
+        }
+    }
+    
+    
+    @IBAction func deleteAction(_ sender: Any) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context: NSManagedObjectContext = appDelegate.persistentContainer.viewContext
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "MentorData")
+        do {
+            let results:NSArray = try context.fetch(request) as NSArray
+            for result in results
+            {
+                let note = result as! MentorData
+                if(note == selectedMentor)
+                {
+                    note.deletedDate = Date()
+                    try context.save()
+                    navigationController?.popViewController(animated: true)
+                }
+            }
+        }
+        catch
+        {
+            print("Fetch Failed")
+        }
+    }
+    
+}
 
+extension ViewController: UIPickerViewDelegate{
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return city[row]
+    }
+}
+
+extension ViewController: UIPickerViewDataSource{
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return city.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        originLabel.text = city[row]
+    }
+    
+    
 }
 
